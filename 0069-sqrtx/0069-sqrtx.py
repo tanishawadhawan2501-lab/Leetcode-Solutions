d8b30bd12a91a8d733fc4e42d19a8ddf9e60c2be
tanishawadhawan2501-lab <tanishawadhawan2501@gmail.com>
2026-08-21 14:15:50 +0530

    Time: 3 ms (58.05%), Space: 12.4 MB (18.62%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        if x < 2:
+            return x
+
+        left = 1
+        right = x // 2
+
+        while left <= right:
+            mid = (left + right) // 2
+
+            if mid * mid == x:
+                return mid
+            elif mid * mid < x:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        return right
