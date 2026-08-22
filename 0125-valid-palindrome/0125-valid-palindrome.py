81442ec5dffdd815efc5ab85591ae9a1fadd4617
tanishawadhawan2501-lab <tanishawadhawan2501@gmail.com>
2026-08-22 21:14:30 +0530

    Time: 19 ms (47.58%), Space: 14.2 MB (16.16%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        s = ''.join(ch.lower() for ch in s if ch.isalnum())
+
+        return s == s[::-1]
